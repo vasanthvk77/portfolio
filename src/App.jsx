c87d3f3314af58a0ae01e5556175320f9dc8e198
Kali Muthu <kalimuthu@cygnussoftwares.com>
2025-12-08 21:34:21 +0530

    Fix: Add basename to BrowserRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from './home.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
